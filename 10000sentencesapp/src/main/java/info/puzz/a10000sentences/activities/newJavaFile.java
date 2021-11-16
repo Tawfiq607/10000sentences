1dbad41c3f91d10562f64d8175159bd5ac669653
Tawfiq607 <amin1707034@stud.kuet.ac.bd>
2021-11-16 19:14:02 +0600

    new java file added

---
+++ 10000sentencesapp/src/main/java/info/puzz/a10000sentences/activities/newJavaFile.java
@@ -0,0 +1,16 @@
+package info.puzz.a10000sentences.activities;
+
+public class newJavaFile {
+
+    int roll;
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+
+}
